7db5d8ccbb0d97cbfc8fa71c3c5f4152cce7552a
Amina <amina.begum.01@gmail.com>
2023-10-18 22:49:10 +0100

    Sign up component created and successful creating of user on backend checked

--- frontend/onTheDoor/App.js
+++ frontend/onTheDoor/App.js
@@ -1,24 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
 import SignUp from './Screens/SignUp'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LogIn from './Screens/LogIn';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
-      <View style={styles.container}>      
-      <SignUp/>
-      </View>
+      <NavigationContainer>   
+        <Stack.Navigator initialRouteName='SignUp'>
+          <Stack.Screen 
+            name='SignUp' 
+            component={SignUp}
+            options={{
+              headerShown: false
+            }}
+            />
+          <Stack.Screen 
+            name='LogIn' 
+            component={LogIn}
+            options={{
+              // headerShown: false, 
+              animationEnabled: false, 
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
-    
-    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: 'mediumspringgreen',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ frontend/onTheDoor/Screens/LogIn.js
@@ -0,0 +1,12 @@
+import { Text, SafeAreaView, TouchableOpacity } from 'react-native';
+import styles from '../Styles/SignUpStyles';
+
+const LogIn = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Log In Page</Text>
+    </SafeAreaView>
+  )
+}
+
+export default LogIn;
