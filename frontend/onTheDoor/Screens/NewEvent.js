2ceeef52ba5573f5366007fffc8f01635e6542d7
Amina <amina.begum.01@gmail.com>
2023-10-19 18:55:45 +0100

    Basic structure of frontend component created

--- frontend/onTheDoor/Screens/NewEvent.js
+++ frontend/onTheDoor/Screens/NewEvent.js
@@ -1,12 +1,26 @@
-import { Text, SafeAreaView } from 'react-native';
-
+import React from 'react';
+import { Text, SafeAreaView, TextInput, TouchableOpacity, View } from 'react-native';
+import styles from '../Styles/NewEventStyles';
 
 const NewEvent = () => {
   return (
-    <SafeAreaView>
-      <Text>New Event Screen</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Name of event</Text>
+      <TextInput placeholder='Event Name' style={styles.inputFields} />
+      <View style={styles.uploadContainer}>
+        <Text style={styles.text}>Upload CSV file</Text>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Select CSV File</Text>
+        </TouchableOpacity>
+        <View>
+          <Text style={styles.text}>Selected File: </Text>
+          <TouchableOpacity style={styles.uploadButton}>
+            <Text style={styles.buttonText}>Upload</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
 export default NewEvent;
---
+++ frontend/onTheDoor/Styles/NewEventStyles.js
@@ -0,0 +1,58 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'mediumspringgreen',
+  },
+  inputFields: {
+    margin: 10,
+    padding: 5,
+    height: 50,
+    width: 200,
+    borderWidth: 1,
+    borderRadius: 6,
+    fontSize: 22,
+    textAlign: 'center',
+    backgroundColor: '#fff',
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+    padding: 5,
+    height: 200,
+    width: 200,
+    borderRadius: 100,
+    backgroundColor: 'dodgerblue',
+  },
+  uploadButton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+    padding: 5,
+    height: 40,
+    width: 100,
+    borderWidth: 1,
+    borderRadius: 6,
+    backgroundColor: 'dodgerblue',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 20,
+    textAlign: 'center'
+  },
+  uploadContainer: {
+    flex: 0.5, 
+    justifyContent: 'center', 
+    alignItems: 'center'
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 18,
+  }
+});
+
+export default styles;
