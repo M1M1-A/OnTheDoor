d2caa1da4ba013bce8a33a2a89d2b515319d4381
Amina <amina.begum.01@gmail.com>
2023-10-20 15:59:23 +0100

    Added functionality to frontend to upload only csv files. Next - add handling the sending of file to backend through fetch

--- frontend/onTheDoor/Screens/NewEvent.js
+++ frontend/onTheDoor/Screens/NewEvent.js
@@ -1,26 +1,38 @@
 import React, { useState } from 'react';
 import { Text, SafeAreaView, TextInput, TouchableOpacity, View } from 'react-native';
+import * as DocumentPicker from 'expo-document-picker';
 import styles from '../Styles/NewEventStyles';
-import DocumentPicker from 'react-native-document-picker';
 
 const NewEvent = () => {  
-  const [file, setFile] = useState()
+  const [file, setFile] = useState(null);
 
   const handleFileUpload = async () => {
     try {
-      const res = await DocumentPicker.pick({
-        type: [DocumentPicker.types.csv]
+      let result = await DocumentPicker.getDocumentAsync({
+        type: 'text/csv',
       });
-      setFile(res);
-
-    } catch(err) {
-      if (DocumentPicker.pick.isCancel(err)) {
-        console.log("User cancelled the picker")
-      }
+      setFile({
+        name: result.assets[0].name,
+        uri: result.assets[0].uri
+      });
+    } catch (err) {
+      console.log('Error', err);
     }
   }
 
   console.log("FILE", file)
+
+  const handleUpload = () => {
+    if (file) {
+      // Handle file upload logic here
+      console.log('Uploading file:', file.uri);
+      // Call your backend API here for uploading the file
+    } else {
+      console.log('No file selected');
+    }
+  };
+
+  console.log('FILE', file);
 
   return (
     <SafeAreaView style={styles.container}>
@@ -28,15 +40,17 @@ const NewEvent = () => {
       <TextInput placeholder='Event Name' style={styles.inputFields} />
       <View style={styles.uploadContainer}>
         <Text style={styles.text}>Upload CSV file</Text>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText} onPress={handleFileUpload}>Select CSV File</Text>
+        <TouchableOpacity style={styles.button} onPress={handleFileUpload}>
+          <Text style={styles.buttonText}>Select CSV File</Text>
         </TouchableOpacity>
-        <View>
-          <Text style={styles.text}>Selected File: </Text>
-          <TouchableOpacity style={styles.uploadButton}>
-            <Text style={styles.buttonText}>Upload</Text>
-          </TouchableOpacity>
-        </View>
+        {file && (
+          <View>
+            <Text style={styles.text}>Selected File: {file.name}</Text>
+            <TouchableOpacity style={styles.uploadButton} onPress={handleUpload}>
+              <Text style={styles.buttonText}>Upload</Text>
+            </TouchableOpacity>
+          </View>
+        )}
       </View>
     </SafeAreaView>
   );
