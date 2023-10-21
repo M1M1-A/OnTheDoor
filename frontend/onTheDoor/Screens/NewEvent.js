09c441340cb96791c60272edf2785d11c28dcbea
Amina <amina.begum.01@gmail.com>
2023-10-21 20:45:14 +0100

    Csv file can be selected and sent to backend. Currently it's sending back in the correct format too. Need to handle temp dowload of file to read data on backend. fs fails as it cannot locate file for given uri

--- frontend/onTheDoor/Screens/NewEvent.js
+++ frontend/onTheDoor/Screens/NewEvent.js
@@ -2,42 +2,62 @@ import React, { useState } from 'react';
 import { Text, SafeAreaView, TextInput, TouchableOpacity, View } from 'react-native';
 import * as DocumentPicker from 'expo-document-picker';
 import styles from '../Styles/NewEventStyles';
+import * as FileSystem from 'expo-file-system';
 
 const NewEvent = () => {  
   const [file, setFile] = useState(null);
+  const [eventName, setEventName] = useState("")
+
+  const handleEventInput = (text) => {
+    setEventName(text);
+  }
 
   const handleFileUpload = async () => {
     try {
       let result = await DocumentPicker.getDocumentAsync({
         type: 'text/csv',
       });
-      setFile({
-        name: result.assets[0].name,
-        uri: result.assets[0].uri
-      });
+      console.log("RESULT", result)
+      setFile(result);
     } catch (err) {
       console.log('Error', err);
     }
   }
 
-  console.log("FILE", file)
-
-  const handleUpload = () => {
+  const handleUpload = async () => {
     if (file) {
-      // Handle file upload logic here
-      console.log('Uploading file:', file.uri);
-      // Call your backend API here for uploading the file
+      try {
+        const response = await fetch('http://192.168.0.12:8080/new-event', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            eventName: eventName,
+            file: file
+          })
+        });
+  
+        if (response.ok) {
+          console.log('Upload successful');
+        } 
+      } catch (error) {
+        console.error('Error uploading file:', error);
+      }
     } else {
       console.log('No file selected');
     }
   };
-
-  console.log('FILE', file);
+  
 
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.text}>Name of event</Text>
-      <TextInput placeholder='Event Name' style={styles.inputFields} />
+      <TextInput 
+            placeholder='Event Name' 
+            style={styles.inputFields}
+            onChangeText={handleEventInput}
+      />
       <View style={styles.uploadContainer}>
         <Text style={styles.text}>Upload CSV file</Text>
         <TouchableOpacity style={styles.button} onPress={handleFileUpload}>
